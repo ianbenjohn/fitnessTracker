8c602091ff1d6fe777a40b7693600ef5fe3065fb
Ian J <ianbennettjohnson@gmail.com>
2020-05-20 16:07:39 -0700

    messing with api.js

--- routes/api.js
+++ routes/api.js
@@ -30,6 +30,9 @@ router.get("/api/workouts", (req, res) => {
     Workout.find()
     .then(dbWorkout => {
         res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
     });
 });
 
