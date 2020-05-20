ce3a638347e96411b32aec4a64f80887c2a148b9
Ian J <ianbennettjohnson@gmail.com>
2020-05-20 13:52:32 -0700

    defined Schema in workout.js and updated server.js routes

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 //routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+app.use(require("./routes/api"));
+app.use(require("./routes/view"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`)
