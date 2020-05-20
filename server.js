f09f9e50119fa2294f656ad7722e7cd86afed0c2
Ian J <ianbennettjohnson@gmail.com>
2020-05-20 13:16:19 -0700

    Added index.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 //routes
